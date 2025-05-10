868b81cd840812fd32431ed6d8f4a29a1f2551af
AYMANE <aymanerachid47@gmail.com>
2025-05-10 01:01:25 +0100

    Replace node-sass with sass, fix build issues

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
